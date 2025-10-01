81f2c39b7a9cc95f8ae18a9442b88fbbb75221eb
Mark <markmccoid@gmail.com>
2025-09-30 23:39:41 -0400

    version bump

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
   expo: {
     name: "laabs",
     slug: "laabs",
-    version: "1.0.2",
+    version: "1.0.3",
     orientation: "portrait",
     icon: "./assets/LAABABS-Icon.icon",
     scheme: "laabs",
